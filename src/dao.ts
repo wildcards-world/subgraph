bb9acd42d86f3876c8b2dbd9eb2d74e344de8c70
moose-code <jonjonclark@gmail.com>
2020-04-26 14:53:48 +0000

    Added dao

---
+++ src/dao.ts
@@ -0,0 +1,7 @@
+import { log } from "@graphprotocol/graph-ts";
+import { LogVote } from "../generated/Dao/Dao";
+import { LogFundsDistrubuted } from "../generated/Dao/Dao";
+
+export function handleLogVote(event: LogVote): void {}
+
+export function handleLogFundsDistrubuted(event: LogFundsDistrubuted): void {}
